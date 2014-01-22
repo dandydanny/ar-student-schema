c6c8e6e1b825281f1ae540e2d5f02ecaa882420f
Apprentice <apprentice@dbc-chicago034.local>
2014-01-22 16:45:21 -0600

    Passed all tests!

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,21 @@
 require_relative '../../db/config'
 
+require 'date'
+# require 'time'
+
 class Student < ActiveRecord::Base
 # implement your Student model here
+
+  def name
+    self.first_name + " " + self.last_name
+  end
+
+  def age
+    ((Date.today - self.birthday) / 365).to_i
+  end
+
+  validates :email, uniqueness: true
+  validates :phone, format: { with: /(\D*\d){10}/}
+  validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/}
+  validates :age, inclusion: { in: 6..999 }
 end
